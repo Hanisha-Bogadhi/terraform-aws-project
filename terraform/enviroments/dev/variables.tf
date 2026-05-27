3eb4e552d4879392af2bc22094220ecb5a0749c0
Hanisha-Bogadhi <hanishabogadhi@gmail.com>
2026-05-27 18:19:50 +0530

    Fix terraform variables and configuration

---
+++ terraform/enviroments/dev/variables.tf
@@ -0,0 +1,19 @@
+variable "username" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
